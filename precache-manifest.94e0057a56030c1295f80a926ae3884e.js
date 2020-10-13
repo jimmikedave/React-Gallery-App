c1babd6c55d0b0eb7c0759b744e85cb5758f0184
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2020-10-13 10:08:05 -0400

    Updates

---
+++ precache-manifest.94e0057a56030c1295f80a926ae3884e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "130da6ac0c3abf954d8ccb4995708532",
+    "url": "/React-Gallery-App/index.html"
+  },
+  {
+    "revision": "2471f9819fae0d92ab02",
+    "url": "/React-Gallery-App/static/css/main.8325ea2a.chunk.css"
+  },
+  {
+    "revision": "53169ab532d4f6b9b580",
+    "url": "/React-Gallery-App/static/js/2.a52ad52d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Gallery-App/static/js/2.a52ad52d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2471f9819fae0d92ab02",
+    "url": "/React-Gallery-App/static/js/main.2a7c89a8.chunk.js"
+  },
+  {
+    "revision": "55cafe36eff79cae7b5d",
+    "url": "/React-Gallery-App/static/js/runtime-main.44333999.js"
+  }
+]);
